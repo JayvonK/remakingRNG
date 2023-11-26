bfce6942f5b365b3266500a9471cd5d0637bce2e
JayvonK <jaykhuth@gmail.com>
2023-11-26 03:22:28 -0800

    save

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,20 @@
 let addBtn = document.getElementById("addBtn");
 let randBtn = document.getElementById("randBtn");
 let deleteBtn = document.getElementById("deleteBtn");
+let userInput = document.getElementById("userInput");
+let nameTxt = document.getElementById("nameTxt");
+
+let nameArray = [];
+
+
+addBtn.addEventListener('click', function(){
+    nameArray.push(userInput.value);
+    console.log(nameArray);
+    userInput.value = "";
+})
+
+randBtn.addEventListener('click', function(){
+    nameTxt.textContent = RandomName();
+})
+
 
---
+++ scripts/randomFunction.js
@@ -0,0 +1,4 @@
+function RandomName(array) {
+    let index = Math.floor(Math.random() * array.length);
+    return 
+}
